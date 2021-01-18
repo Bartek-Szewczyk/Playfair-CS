ef6fcd1bd5459891541f6a420fca536120cedca1
Bartek-Szewczyk <ba20001000@gmail.com>
2021-01-18 03:39:09 +0100

    done

--- Playfair/Playfair/MainWindow.xaml.cs
+++ Playfair/Playfair/MainWindow.xaml.cs
@@ -24,5 +24,195 @@ namespace Playfair
         {
             InitializeComponent();
         }
+
+
+        private void wykonaj_Click(object sender, RoutedEventArgs e)
+        {
+
+            string klucz = key.Text.Replace(" ","").ToLower().Replace("j","i");
+            string tekstJawny = text.Text.Replace(" ","").ToLower().Replace("j", "i");
+            
+
+            if (tekstJawny.Length % 2 != 0)
+            {
+                tekstJawny += 'z';
+            }
+
+
+
+            char[,] matrixKw = new char[5, 5];
+            string kontrolny = "";
+            string alfabet = "abcdefghiklmnopqrstuvwxyz";
+
+            int x = 0, y = 0;
+            int pozycja;
+
+            foreach (char znak in klucz.ToCharArray())
+            {
+                pozycja = kontrolny.IndexOf(znak);
+                if (pozycja == -1)
+                {
+                    kontrolny += znak;
+                    matrixKw[x, y] = znak;
+                    x++;
+                    if (x == 5)
+                    {
+                        x = 0;
+                        y++;
+                    }
+                }
+            }
+
+            foreach (char znak in alfabet.ToCharArray())
+            {
+                pozycja = kontrolny.IndexOf(znak);
+                if (pozycja == -1)
+                {
+                    kontrolny += znak;
+                    matrixKw[x, y] = znak;
+                    x++;
+                    if (x == 5)
+                    {
+                        x = 0;
+                        y++;
+                    }
+                }
+            }
+
+            if (szyfruj.IsSelected)
+            {
+
+
+                for (int j = 0; j < tekstJawny.Length; j += 2)
+                {
+                    char firstLetter = tekstJawny[j];
+                    char secondLetter = tekstJawny[j + 1];
+
+                    int firstX = 0, firstY = 0, secondX = 0, secondY = 0;
+
+                    for (int row = 0; row < 5; row++)
+                    {
+                        for (int col = 0; col < 5; col++)
+                        {
+                            if (firstLetter == matrixKw[row, col])
+                            {
+                                firstX = row;
+                                firstY = col;
+                            }
+
+                            if (secondLetter == matrixKw[row, col])
+                            {
+                                secondX = row;
+                                secondY = col;
+                            }
+                        }
+                    }
+
+                    if (firstX == secondX)
+                    {
+                        szyfr.Text += matrixKw[firstX, (firstY + 1) % 5];
+                        szyfr.Text += matrixKw[secondX, (secondY + 1) % 5];
+
+                    }
+                    else if (firstY == secondY)
+                    {
+                        szyfr.Text += matrixKw[(firstX + 1) % 5, firstY];
+                        szyfr.Text += matrixKw[(secondX + 1) % 5, secondY];
+                    }
+                    else
+                    {
+                        szyfr.Text += matrixKw[secondX, firstY];
+                        szyfr.Text += matrixKw[firstX, secondY];
+                    }
+                }
+
+            }
+            else
+            {
+
+                for (int j = 0; j < tekstJawny.Length; j += 2)
+                {
+                    char firstLetter = tekstJawny[j];
+                    char secondLetter = tekstJawny[j + 1];
+
+
+                    int firstX = 0, firstY = 0, secondX = 0, secondY = 0;
+
+                    for (int row = 0; row < 5; row++)
+                    {
+                        for (int col = 0; col < 5; col++)
+                        {
+                            if (firstLetter == matrixKw[row, col])
+                            {
+                                firstX = row;
+                                firstY = col;
+                            }
+
+                            if (secondLetter == matrixKw[row, col])
+                            {
+                                secondX = row;
+                                secondY = col;
+                            }
+                        }
+                    }
+
+                    if (firstX == secondX)
+                    {
+                        if (firstY - 1 < 0)
+                        {
+                            szyfr.Text += matrixKw[firstX, 4];
+                        }
+                        else
+                        {
+                            szyfr.Text += matrixKw[firstX, firstY - 1];
+                        }
+
+                        if (secondY - 1 < 0)
+                        {
+                            szyfr.Text += matrixKw[secondX, 4];
+                        }
+                        else
+                        {
+                            szyfr.Text += matrixKw[secondX, secondY - 1];
+                        }
+
+
+
+                    }
+                    else if (firstY == secondY)
+                    {
+                        if (firstX - 1 < 0)
+                        {
+                            szyfr.Text += matrixKw[4, firstY];
+                        }
+                        else
+                        {
+                            szyfr.Text += matrixKw[firstX - 1, firstY];
+                        }
+
+                        if (secondX - 1 < 0)
+                        {
+                            szyfr.Text += matrixKw[4, secondY];
+                        }
+                        else
+                        {
+                            szyfr.Text += matrixKw[secondX - 1, secondY];
+                        }
+                    }
+                    else
+                    {
+                        szyfr.Text += matrixKw[secondX, firstY];
+                        szyfr.Text += matrixKw[firstX, secondY];
+                    }
+                }
+
+
+
+
+
+
+            }
+
+        }
     }
 }
